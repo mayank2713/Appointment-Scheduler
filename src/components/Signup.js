d6c78deee7e595ce7682a7fe7a1415762d468b09
Mayank chourey <99243433+mayank2713@users.noreply.github.com>
2025-07-22 01:24:18 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -8,7 +8,7 @@ const Signup = (props) => {
         e.preventDefault();
         fetch("")
         const {name, email, password} = credentials;
-        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+        const response = await fetch("https://appointment-scheduler-4-deb5.onrender.com/api/auth/createuser", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'            },
