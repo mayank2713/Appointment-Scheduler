5de910df82d60d6f3260d87c601024b4ccf0b435
Mayank chourey <99243433+mayank2713@users.noreply.github.com>
2025-07-22 01:23:47 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ const Login = (props) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         fetch("")
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+        const response = await fetch("https://appointment-scheduler-4-deb5.onrender.com/api/auth/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'            },
